d236d697ea35c7df416d11b1bba707a04bcd73a3
NastyaZ23 <nastyh1233@gmail.com>
2021-10-21 10:43:57 +0300

    Updates

---
+++ precache-manifest.5589dac2357ebb37a32be0620632347b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89eb09120a98a32c5e269154fd012b54",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "edf8c94be9787eb6bc99",
+    "url": "/TodoList/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "db4ae90904015ac8bc24",
+    "url": "/TodoList/static/js/2.fadeb374.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.fadeb374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf8c94be9787eb6bc99",
+    "url": "/TodoList/static/js/main.ac437b5e.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
