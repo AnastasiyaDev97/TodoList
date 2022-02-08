bd7a19ae93bac2b16dc54277d585dbb8292b9090
NastyaZ23 <nastyh1233@gmail.com>
2022-02-08 17:39:30 +0300

    add hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import AppWithRedux from "./AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 
 
 ReactDOM.render(
      <Provider store={store}>
-             <BrowserRouter>
-                     <AppWithRedux /> </BrowserRouter> </Provider>
+             <HashRouter>
+                     <AppWithRedux /> </HashRouter> </Provider>
     ,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
